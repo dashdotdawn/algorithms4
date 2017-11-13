3884ba1cb4fe488fc5a85e5d0b1e79f3c38a36d3
dawn <zsc_1992@163.com>
2017-11-13 23:51:35 +0800

    improve percolation

--- Percolation.java
+++ Percolation.java
@@ -10,11 +10,6 @@ public class Percolation {
     private boolean[][] grid;
     private int openSitesCount;
 
-    private void connectSites(int centerIndex, int row, int col) {
-        if (isOpen(row, col)) {
-            uf.union(centerIndex, getIndex(row, col));
-        }
-    }
     public Percolation(int n) {
         if (n <= 0 ) throw new IllegalArgumentException("n should be greater than zero.");
         size = n;
@@ -23,11 +18,17 @@ public class Percolation {
         virtualBottom = n * n + 1;
         grid = new boolean[n][n]; // default value false
         openSitesCount = 0;
-        for (int i = n; i > 0; i--) {
-            uf.union(virtualTop, i);
-            uf.union(virtualBottom, n * (n - 1) + i);
-        }
     }
+
+    private int getIndex(int row, int col) {
+        return (row - 1) * size + col;
+    }    
+    private void connectSites(int centerIndex, int row, int col) {
+        if (row == 0) uf.union(centerIndex, virtualTop);
+        else if (row == size + 1) uf.union(centerIndex, virtualBottom);
+        else if (isOpen(row, col)) uf.union(centerIndex, getIndex(row, col));
+    }
+
     public void open(int row, int col) {
         if (row <= 0 || row > size || col <= 0 || col > size) {
             throw new IllegalArgumentException("the row and column indices are integers between 1 and n");
@@ -36,8 +37,8 @@ public class Percolation {
             int index = getIndex(row, col);
             grid[row - 1][col - 1] = true;
             openSitesCount++;
-            if (row - 1 > 0) connectSites(index, row - 1, col);
-            if (row + 1 <= size) connectSites(index, row + 1, col);
+            if (row - 1 >= 0) connectSites(index, row - 1, col);
+            if (row + 1 <= size + 1) connectSites(index, row + 1, col);
             if (col - 1 > 0) connectSites(index, row, col - 1);
             if (col + 1 <= size) connectSites(index, row, col + 1);
         }
@@ -53,7 +54,7 @@ public class Percolation {
         if (row <= 0 || row > size || col <= 0 || col > size) {
             throw new IllegalArgumentException("the row and column indices are integers between 1 and n");
         }
-        return uf.connected(virtualTop, getIndex(row, col)) || uf.connected(virtualBottom, getIndex(row, col));
+        return isOpen(row, col) && uf.connected(virtualTop, getIndex(row, col));
     }
     public int numberOfOpenSites() {
         return openSitesCount;
@@ -61,9 +62,7 @@ public class Percolation {
     public boolean percolates() {
         return uf.connected(virtualTop, virtualBottom);
     }
-    private int getIndex(int row, int col) {
-        return (row - 1) * size + col;
-    }    
+
     public static void main(String[] args) {
         int n = StdIn.readInt();
         Percolation percolation = new Percolation(n);
@@ -71,6 +70,7 @@ public class Percolation {
             int p = StdIn.readInt();
             int q = StdIn.readInt();
             percolation.open(p, q);
+            if (percolation.numberOfOpenSites() == 231) StdOut.println(percolation.isFull(18, 1));
         }
         StdOut.println(percolation.numberOfOpenSites() + " open sites");
         StdOut.println(percolation.percolates());
